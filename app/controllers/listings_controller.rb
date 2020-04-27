21ee0a912c4e346103db398c1bbca93545378251
vipul022 <vipul.seth022@gmail.com>
2020-04-27 15:43:15 +1000

    added controllers and methods, plus views for listings

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,29 @@
+class ListingsController < ApplicationController
+    def create
+        #create new listing
+    end
+
+    def update
+        #update the current listing
+    end
+
+    def index
+        #show all listings
+    end
+
+    def edit
+        #shows the edit form
+    end
+
+    def new
+        #shows a form for creating new listing
+    end
+
+    def destroy
+        #deleted current listing
+    end
+
+    def show
+        #show the single listing
+    end
+end
