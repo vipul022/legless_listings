9b02dfd53128695f762e4f08478ccf2e262c11a4
vipul022 <vipul.seth022@gmail.com>
2020-04-28 11:22:30 +1000

    added a form to the new view

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,29 +1,45 @@
 class ListingsController < ApplicationController
+    before_action :set_listing, only: [:show, :edit, :update, :destroy]
+
     def create
         #create new listing
     end
 
     def update
         #update the current listing
+ 
     end
 
     def index
         #show all listings
+        @listings = Listing.all
     end
 
     def edit
         #shows the edit form
+        
     end
 
     def new
         #shows a form for creating new listing
+        @listing = Listing.new
     end
 
     def destroy
         #deleted current listing
+        
     end
 
     def show
         #show the single listing
+       
     end
+
+    private
+
+    def set_listing
+        id = params[:id]
+        @listing = Listing.find(id)
+    end
+
 end
